01cd3679d1f6bf108fb16d63d1a2ddc031a932c3
Deise Trianon <deisetrianon@gmail.com>
2018-07-17 14:58:30 -0300

    adicionamos nomes,fotos e status

--- js/estudantes.js
+++ js/estudantes.js
@@ -43,16 +43,35 @@ function dataStudents() {
   studentsProfiles.innerHTML = "";
 
   for (i in data[sede][turma]['students']) {
+    /* Cria a div que vai receber as informações das alunas */
     var profileBox = document.createElement("div");
-    var names = data[sede][turma]['students'][i]['name'];
+    profileBox.style = " align-self: center; width: 700px; height: auto; background-color: white; margin: 20px; border: 1px solid rgb(209, 209, 209); padding: 10px;";
+
+    /* Insere o nome da aluna */
+    var names = document.createElement("p");
+    names.innerHTML = data[sede][turma]['students'][i]['name'] + '<hr class="hr-data-title"></hr>';
+    names.style = "text-transform: uppercase; color: black; font-family: 'Lato', sans-serif; font-weight: bold; font-size: 17px; letter-spacing: 0;"
+
+    /* Insere as imagens das alunas */
     var img = document.createElement("img");
     var photoStudents = data[sede][turma]['students'][i]['photo'];
     img.setAttribute('src', photoStudents);
+    img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/1/19/Gray_-_replace_this_image_female.svg');
+    img.style = "width = 132px; height: 125px; float: left";
 
-    profileBox.innerHTML = names;
+    /* Insere o status das alunas */
+    var status = document.createElement("p");
+    status.style = "font-family: 'Lato', sans-serif; font-size: 11px; color: black; font-weight: normal; text-align: center";
+    if (data[sede][turma]['students'][i]['active'] === true) {
+      status.innerHTML = 'ATIVA: SIM';
+    } else {
+      status.innerHTML = 'ATIVA: NÃO';
+    }
+
+    profileBox.appendChild(names);
     profileBox.appendChild(img);
-    profileBox.setAttribute('class', 'profileBox');
+    profileBox.appendChild(status);
     studentsProfiles.appendChild(profileBox);
-  }
 
+  }
 }
